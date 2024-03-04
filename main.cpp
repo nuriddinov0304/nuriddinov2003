6c79574297d7f8a6201e8f70b4fcd5f322f67f07
nuriddinov2003 <nuriddinovavazbek5@gmail.com>
2024-03-04 14:32:42 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,574 @@
+
+    /* cout << " (0,0) (0,1) (0,2) (0,3) (0,4)  " <<  endl;
+     cout <<      " (0,0) (0,1) (0,2) (0,3) (0,4)  " <<  endl;
+     cout <<       " (0,0) (0,1) (0,2) (0,3) (0,4)  "<<  endl;
+     cout <<      " (0,0) (0,1) (0,2) (0,3) (0,4)   "<<  endl;*/ //1 misol
+    /*cout << "                    @              " << endl;
+    cout << "                    @@@@                " << endl;
+    cout << "                   @@@@@@@                " << endl;
+    cout << "                  @@@@@@@@@@                " << endl;
+    cout << "                  @@@@@@@@@@@@                " << endl;
+    cout << "                  @@@@@@@@@@                " << endl;
+    cout << "                   @@@@@@@@                " << endl;
+    cout << "            @@@      @@@@       @@@          " << endl;
+    cout << "         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << "       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               " << endl;
+    cout << "     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << "   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << "  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               " << endl;
+    cout << "      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                " << endl;
+    cout << "        @@@@@@@  @@@@@@@   @@@@@@@                " << endl;
+    cout << "         @@@@@    @@@@@     @@@@@                " << endl;*/  //-2-MISOL
+    /* cout<<"    XXXXXXXXXXXXX              XX                         XX             XXXXXXXXXXX        X     XXXXX     "<<endl;
+     cout<<"   XXX         XXX          XXXXX                      XXXXX            XXX       XXX      XX         X"<<endl;
+     cout<<"  XX         XXX               XX                         XX           XX       XXX       X  X       X   "<<endl;
+     cout<<"            XXX                XX               XX        XX                   XXX       X   X      X  "<<endl;
+     cout<<"          XXX                  XX               XX        XX                 XXX        XXXXXXX    X     "<<endl;
+     cout<<"        XXX                    XX                         XX               XXX              X      X        "<<endl;
+     cout<<"       XXX                     XX                         XX              XXX               X      X    "             <<endl;
+     cout<<"      XXX                      XX                         XX             XXX                     "<<endl;
+     cout<<"    XXX                        XX               XX        XX            XXX                        "<<endl;
+     cout<<"   XX                          XX               XX        XX            XX                         "<<endl;
+     cout<<"   XX                          XX                         XX            XX                   "<<endl;
+     cout<<"   XXXXXXXXXXXXXX       XXXXXXXXXXXXX             XXXXXXXXXXXXXXXX    XXXXXXXXXXXXX                                        "<<endl;
+     cout<<"                                dushanba 2024 fevral 21:12                                                                                              "<<endl;*/ // 3 misil
+    /* cout <<"             88888         88888888888888              888           8888888                                       "<< endl;
+      cout <<"         888     888            88                  88   88         88    888                      "<< endl;
+      cout <<"       88        888            88                888     888       88      888                         "<< endl;
+      cout <<"                888             88               888       888      88      88                            "<< endl;
+      cout <<"              888               88              888        888      8888888888                            "<< endl;
+      cout <<"             888                88              888        888      888                                "<< endl;
+      cout <<"           888                  88              88888888888888      88 88                              "<< endl;
+      cout <<"         888        88          88              888        888      88   88                                        "<< endl;
+      cout <<"          888    888            88              888        888      88    88                                   "<< endl;
+      cout <<"             88888              88              888        888      88     88                                 "<< endl;
+      cout <<"                                                                                                    "<< endl;
+      cout <<"         888        888         888        888       888888         88888                                                 "<< endl;
+      cout << "        888        888         888      88   88     88   888     888   888                                                      " << endl;
+      cout << "        888        888         888     888    888   88     88   88      888                                                     " << endl;
+      cout << "        888       88888        888     888    888   88     88           888                                                  " << endl;
+      cout << "        888      88    88      888     8888888888   88888888           888                                                   " << endl;
+      cout << "        888   88888888888888   888     888    888   88 8             888     88                                            " << endl;
+      cout << "        888  88            88  888     888    888   88  88          888    888                                                   " << endl;
+      cout << "        888 88              88 888     888    888   88    88           88888                                                    " << endl;*/  // 4 misol
+/*int myAge = 21;
+cout <<myAge<< endl;
+string myName = "Avazbek";
+cout <<myName<< endl;
+string myFamily = "Nuriddinov";
+cout <<myFamily<< endl;
+string myAdress = "Samarqand";
+cout <<myAdress<< endl;
+double boy = (1.77);
+ cout <<boy<< endl;
+ bool voyaga= true;
+ cout<<voyaga<< endl;/*/  // ozim haqimda ma'lumot
+/*string ism;
+cout <<"ismingizni kiriting:";
+cin >>ism;
+cout <<"assalomu alekum xush kelibsiz "<<ism << endl;
+int yosh;
+cout <<"yoshingizni kiriting:";
+cin >> yosh;
+cout <<"siz "<<2024-yosh << endl;*/
+/*int a,b;
+cout << "a:";
+cin >> a;
+cout << "b:";
+cin >> b;
+int summa = a+b;
+cout <<"summa="<<summa<<endl;
+int kopaytma = a*b;
+    cout <<"kopaytma="<<kopaytma<<endl;
+int ayirma = a-b;
+    cout <<"ayirma="<<ayirma<<endl;*/
+/*int a,b;
+cout << "a:";
+cin >> a;
+cout << "b:";
+cin >>b;
+int yigindi = a+b;
+
+cout <<"yigindi: "<<yigindi<< endl;*/  // 3 dars vazifalari 1 milol
+/*int a;
+cout << "a ni kiriting:";
+cin >> a;
+int peremetr = 4*a;
+cout <<"peremetr:"<<peremetr<<endl;*/  // 2 misol
+/*int a;
+cout << "a ni kiriting:";
+cin >> a;
+int yuza = a*a;
+cout <<"yuza:"<<yuza<<endl;*/  // 3 misol
+/*int a,b;
+cout << "a kiriting:";
+cin >> a;
+cout <<"b kiriting:";
+cin >> b;
+int yuza = a*b;
+int peremetr = 2*(a+b);
+cout <<"yuza:"<<yuza<<endl;
+cout <<"peremetr"<<peremetr<<endl;*/  // 4 misol
+/*int a;
+cout <<"a kiriting:";
+cin >> a;
+int hajm = a*a*a;
+int yuza = 6*a*a;
+cout <<"hajm:"<<hajm<<endl;
+cout <<"yuza:"<<yuza<<endl;*/  // 5 misol
+/*double a,b;
+cout <<"a:";
+cin >> a;
+cout <<"b:";
+cin >> b;
+double arfimetik = (a+b)/2;
+cout <<"orta arfimetik:"<<arfimetik<<endl;*/  // 6 misol
+   /* int a, b;
+    std::cout << "a va b ni kiriting: ";
+    std::cin >> a >> b;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    std::cout << "Almashtirilgan qiymatlar: " << a << " " << b << std::endl;*/
+   /*int a,b;
+   cout <<"a va b ni qiymatini kiriting:";
+   cin >>a>>b;
+   int dood = a;
+   a=b;
+   b=dood;
+   cout <<"almashtirish:"<< a << b <<endl;*/  // 7 misol
+  /* int A,B,C;
+   cout <<"A va B va C ni qiymatlarini kiriting:";
+    cin >>A>>B>>C;
+   int temp = A;
+   A=B;
+   B=temp;
+   B=C;
+   C=temp;
+   cout <<"sonlar almashinuvi:"<<A<<B<<endl;
+    cout <<"sonla almashinuvi:"<<B<<C<<endl;
+    cout <<"sonl almashinuvi:"<<A<<C<<endl;*/  // 8 misol
+  /*double x,y;
+  cout <<"1 kg konfet narxi:";
+  cin >>x;
+  cout <<"olinishi kerak bolgan konfet:";
+  cin >>y;
+  double formula =x*y;
+  cout <<"konfet summasi:"<<formula<<"so'm"<<endl;*/  // 9 misol
+  /*int x,y,a,b;
+    cout <<"1 kg konfet narxi:";
+    cin >>x;
+    cout <<"konfet kgmi:";
+    cin >>a;
+    int k = x*a;
+    cout <<"konfet tan narxi:"<<k<<endl;
+  cout <<"1 kg shokalad narxi:";
+    cin >>y;
+  cout <<"shokalad kgmi:";
+  cin >>b;
+  int sh = y*b;
+  cout <<"shokakad tan narxi:"<<sh<<endl;
+  int farq =sh-k;
+  cout <<"shokaladni konfetdan qimmatligi:"<<farq<<endl;*/   // 10 misol
+  /*int a;
+  cout <<"a ni qiymati kiritilsin:";
+  cin >>a;
+  int kvadrat =a*a;
+  cout <<"a ning kvadrati:"<<kvadrat<<endl;*/  // 11 misol
+    /*int a;
+    cout <<"a ni qiymati kiritilsin:";
+    cin >>a;
+    int kubi =a*a*a;
+    cout <<"a ning kubi:"<<kubi<<endl;*/  // 12 misol
+    /*int a;
+    cout <<"a ni kiriting:";
+    cin >>a;
+    int kopaytirish =a*a;
+    cout <<"1 amal:"<<kopaytirish<<endl;
+    cin>>a;
+    int kopaytir =a*a;
+    cout <<"2 amal:"<<kopaytir<<endl;
+int umumiy =kopaytir+kopaytirish;
+cout <<"javob:"<<umumiy<<endl;*/  // 13 misol
+   /* int a;
+    cout <<"a ni kiriting:";
+    cin >>a;
+    int daraja =(a)*/
+   /*double N;
+   cout <<"N ni kiriting:";
+   cin >>N;
+   double vaqt =(N*2.5)/5;
+   cout <<"2.5 daqiqada so'zni oqishi:"<<vaqt<<endl;*/  // 15 misol
+   /*double a,b;
+   cout <<"patrikni puki:";
+   cin>>a;
+   cout <<"juliani puli:";
+   cin >>b;
+   double farq =a-b;
+   cout <<"patrikning juliandan pulini kopligi:"<<farq<<endl;
+   double ketishi =(a-b)/2;
+   cout <<"patrik julinga berishi kerak bolgan pul:"<<ketishi<<endl;*/  // 16 misol
+   /*double a,b,c;
+   a=720,b=50,c=152;
+   double mg =(a*b*c)/(1024*1024);
+   cout <<"kitob necha MG ekanligi:"<<mg<<" MG "<<endl;*/  // 17 misol
+   /*string a,b,c,d,e,f;
+    cout <<"ismingizni kiriting:";
+    cin >>a;
+    cout <<"Familyangizni kiriting:";
+    cin >>b;
+    cout <<"Elektiron pochtangizni kiriting:";
+    cin >>c;
+    cout <<"Telefon raqamingizni kiriting:";
+    cin >>d;
+    cout <<"Manzilingizni kiriting:";
+    cin >>e;
+    cout <<"Jinsingizni kiriting:";
+    cin >> f;
+    cout <<"Mening ismim            : "<<a << endl;
+    cout <<"Mening familyam         :"<<b<< endl;
+    cout <<"Elektiron pochtam       :"<<c<<endl;
+    cout <<"Telefon raqamim         :"<<d<<endl;
+    cout <<"Manzilim                :"<<e<<endl;
+    cout <<"Jinsim                  =>"<<f<<endl;*/
+
+       /* int a;
+        cout << "a:";
+        cin >> a;
+        int a3, a6, a12, a15;
+        a3 = a * a * a;
+        a6 = a3 * a3;
+        a12 = a6 * a6;
+        a15 = a12 * a3;
+
+        cout << "a^15 => " << a15 << endl;*/
+
+/*double a,b,c,d;
+    cout <<"a ni kiriting =>";
+    cin >>a;
+    double kurs =a/15500;
+    cout <<"AQSH qiymat:"<<kurs<<endl;
+
+
+    double kur =a/13200;
+    cout <<"EUR qiymat:"<<kur<<endl;
+
+
+    double ku =a/100;
+    cout <<"RUB qiymat:"<<ku<<endl;
+
+
+    double k =a/15500;
+    cout <<"DPG qiymat:"<<k<<endl;*/  // valyuta kurslari
+/*int a;
+cout <<"a:";
+cin>>a;
+int  yuzlik =a/100;
+int onlik =(a/10)%10;
+int birlik =a%10;
+cout <<""<<yuzlik<<endl;
+cout <<""<<onlik<<endl;
+cout <<""<<birlik<<endl;
+main();*/
+/*int a;
+cout <<"a:";
+cin >>a;
+int son =a/10;
+cout <<"onlar xonasi:"<<son<<endl;
+int soo =a%10;
+cout <<"birlar xonasi:"<<soo<<endl;
+int natija =son+soo;
+cout <<"natija:"<<natija<<endl;*/  // 4 dars vazifalari
+/*int a;
+cout <<"a:";
+cin >>a;
+int  birlar =a%10;
+ int onlar =a/10;
+ cout <<""<<birlar<<onlar<<endl;
+ main();*/
+/*int a;
+cout<<"a:";
+cin >>a;
+int d =a/100;
+cout <<""<<d<<endl;*/
+/*int a;
+cout <<"a:";
+cin >>a;
+int qoldiq =a%10;
+cout <<"qoldiq:"<<qoldiq<<endl;
+int onlar =a/10%10;
+cout <<"onlar:"<<onlar<<endl;*/
+/*int n;
+cout<<"a:";
+cin >>n;
+int a,b,c;
+a =n/100,b =n/10%10,c =n%10;
+cout <<"yuzlar:"<<a<<endl;
+cout <<"onlar:"<<b<<endl;
+cout <<"birlar:"<<c<<endl;
+int natija =a+b+c;
+cout <<"javob="<<natija<<endl;*/  //
+/*int m;
+cout<<"a:";
+cin >>m;
+int a,b,c;
+a=m/100,b=m/10%10,c=m%10;
+cout <<""<<c<<b<<a<<endl;*/  //
+    /*int m;
+    cout<<"a:";
+    cin >>m;
+    int a,b,c;
+    a=m/100,b=m/10%10,c=m%10;
+    cout <<""<<b<<c<<a<<endl;*/
+    /*int m;
+    cout <<"m:";
+    cin >>m;
+    int a =m/100%10;
+    cout <<"=>"<<a<<endl;*/
+   /* int a;
+    cout <<"a=";
+    cin >>a;
+    int v =a/60;
+    cout <<"=>"<<v<<endl;
+    main();*/
+   /* int a;
+    cout <<"a=";
+    cin >>a;
+    int v =a/3600;
+    cout <<"=>"<<v<<endl;*/
+    /*int m;
+    cout <<"m=>";
+    cin >>m;
+    int a,b;
+    a=m/60,b=m%60;
+    cout <<"min"<<"=>" <<a<< endl;
+     cout <<"sek"<<"=>" <<b<< endl;*/
+/*int m;
+    cout <<"m=>";
+    cin >>m;
+    int f, a,b,c;
+  f=m/86400,  a=(m%86400)/3600,b=(m%3600)/60,c=((m%3600)%60)%60;
+  cout <<"kun:"<<"=>"<<f<<endl;
+    cout <<"soat"<<"=>" <<a<< endl;
+     cout <<"min"<<"=>" <<b<< endl;
+cout <<"sek"<<"=>"<<c<<endl;*/
+
+/*int a,b,c;
+cout <<"a=>";cin >>a;
+cout <<"b=>";cin >>b;
+cout <<"c=>";cin >>c;
+
+
+int noldanKatta =(a>0)+(b>0)+(c>0);
+cout <<"noldan katta son:"<<noldanKatta<<endl;*/  //
+/*int a,b,c;
+cout <<"a:"<<"b:"<<"c:";
+cin >>a>>b>>c;
+
+int d =(a+b+c)/3;
+cout <<"=>"<<d<<endl;*/  // 1 misol
+/*int a;
+cout <<"a:";
+cin >>a;
+
+int s =a*4;
+cout <<"P:"<<s<<endl;*/  // 2 misol
+/*int a,b;
+cout <<"a:"<<"b:";
+cin >>a>>b;
+
+int s =a*b;
+int  p =(a+b)*2;
+cout <<"S:"<<s<<endl;
+cout <<"P:"<<p<<endl;*/  // 3 misol
+/*int a;
+cout <<"a:";
+cin >>a;
+
+int s =a*a;
+cout <<"s=>"<<s<<endl;
+main();*/  // 6 misol
+/*int a;
+cout <<"a:";
+cin >>a;
+
+int b =a*11200;
+cout <<"a=>"<<b<<endl;*/  // 7 misol
+/*int a;
+cout <<"a:";
+cin >>a;
+
+int w =a/1000,d=a/10%10;
+int g=w*d;
+cout <<"g=>"<<g<<endl;*/  // 8 misol
+/*int a;
+cout<<"a:";
+cin >>a;
+
+int b=a*a*a;
+int  s=b*b*a;
+cout <<"a7:"<<s<<endl;*/  //  10 misol
+/*int a;
+cout <<"a:";
+cin >>a;
+
+int l,m,n;
+l=a/100;m=a/10%10;n=a%10;
+int s=n*100+m*10+l;
+cout <<""<<s<<endl;*/  // 14 misol
+
+
+/*bool a= false,b= true,c= true,d= false;
+bool m=!(((a&&!b)||(c||d))&&!(a||b));
+cout <<boolalpha<<m<<endl;*/
+
+
+/*int a,b;
+cout <<"a,b:";
+cin >>a>>b;
+
+cout <<""<<boolalpha<<((a>0 && b<0)||(a<0 && b>0))<<endl;
+
+main();*/  // 5 misol
+/*int a;
+cout <<"a:";
+cin >>a;
+
+cout<<"=>"<<boolalpha<<(a%2==0)<<endl;*/  // 6 misol
+/*int a;
+cout <<"a:";cin>>a;
+
+cout <<""<<boolalpha<<(a>9 && a<100 && a%2!=0 )<<endl;*/  // 7 misol
+
+/*int m;
+cout <<"m:";cin>>m;
+int a,b,c;
+a=m/100,b=m/10%10,c=m%10;
+cout <<"="<<boolalpha<<(m>99 && m<1000 && a!=b && b!=c && a!=c)<<endl;*/  // 8 misol
+    /*int m;
+    cout <<"m:";cin>>m;
+    int a,b,c;
+    a=m/100,b=m/10%10,c=m%10;
+    cout <<"="<<boolalpha<<(m>99 && m<1000 && a!=b && b!=c && a==c)<<endl;*/ // 9 misol
+    /*int a,b,c;
+    cout <<"a,b,c:";cin>>a>>b>>c;
+
+    cout <<"<<"<<boolalpha<<( a*a+b*b==c*c)<<endl;*/  // 10 misol
+   /* int A,B,C,D;
+    cout <<"a,b,c,d:";
+    cin>>A>>B>>C>>D;
+   cout<< (A+B>C && A+C>B && C+B>A);
+    cout<<(A+B>D && A+D>B && D+B>A);
+    cout<<(A+D>C && A+C>D && C+D>A);
+    cout<<(D+B>C && D+C>B && C+B>D);*/  //11 misol
+   /*int x1,x2;
+   cout <<"x1,x2:";
+   cin >>x1>>x2;
+   cout <<"=>"<<boolalpha<<(  -x1+x2==27 || -x2+x1==27)<<endl;*/  // 12 misol
+
+  /* int a;
+   cout <<"kiriting:";
+   cin>>a;
+    switch (a) {
+        case 1:cout<<"dushanba"<<endl;
+            break;
+        case 2:cout<<"seshanba"<<endl;
+            break;
+        case 3:cout<<"chorshanba"<<endl;
+            break;
+        case 4:cout<<"payshanba"<<endl;
+            break;
+        case 5:cout<<"juma"<<endl;
+            break;
+        case 6:cout<<"shanba"<<endl;
+            break;
+        case 7:cout<<"yakshanba"<<endl;
+            break;*/
+
+
+/*long double summa;
+cout <<"Pul miqdorini kiriting:";
+cin >>summa;
+int vale;
+
+cout <<"Valyuta kurslari"<<endl<<endl;
+cout <<"1. AQSh dollari , USD       12440.00"<<endl;
+cout <<"2 Evro , EUR                13350.00"<<endl;
+cout <<"3 Rossiya rubli , RUB         100.00"<<endl;
+cout <<"4 Angliya funt sterlingi , GBP         15600.00"<<endl;
+cout <<"5 Shveytsariya franki , CHF          13950.00"<<endl;
+cout <<"6 Yaponiya iyenasi , JPY              70.00"<<endl;
+cout <<"7 Qozog‘iston tengesi , KZT            15.00"<<endl;
+cout <<"Kerakli valyutani tanlang(1-7)"<<vale<<endl;
+cin>>vale;
+
+
+    switch (vale) {
+        case 1:cout<<"siz USD ni tanladingiz"<<endl;
+        cout <<"sizning USD qiymatidagi pul miqdiringiz:"<<summa/12440<<endl;
+            break;
+        case 2:cout<<"siz EUR ni tanladingiz"<<endl;
+            cout <<"sizning EUR qiymatidagi pul miqdiringiz:"<<summa/13350<<endl;
+            break;
+        case 3:cout<<"siz RUB ni tanladingiz"<<endl;
+            cout <<"sizning RUB qiymatidagi pul miqdiringiz:"<<summa/100<<endl;
+            break;
+        case 4:cout<<"siz GPB ni tanladingiz"<<endl;
+            cout <<"sizning GPB qiymatidagi pul miqdiringiz:"<<summa/15600<<endl;
+            break;
+        case 5:cout<<"siz CHF ni tanladingiz"<<endl;
+            cout <<"sizning CHF qiymatidagi pul miqdiringiz:"<<summa/13950<<endl;
+            break;
+        case 6:cout<<"siz JPY ni tanladingiz"<<endl;
+            cout <<"sizning JPY qiymatidagi pul miqdiringiz:"<<summa/70<<endl;
+            break;
+        case 7:cout<<"siz KZI ni tanladingiz"<<endl;
+            cout <<"sizning KZI qiymatidagi pul miqdiringiz:"<<summa/15<<endl;
+            break;
+    }*/  // valyuta kurslari
+
+/*int parol = 1222;
+long long int balans=400000;
+int ozbektili,rusbningliz;
+
+
+
+cout<<"\n\t\t\tKirish"<<endl<<endl;
+cout<<"\t\t1 O'zbek tili"<<endl;
+cout<<"\t\t2 Rus tili"<<endl;
+cout<<"\t\t3 Ingliz tili"<<endl;
+cout<<"\n\t\tKerakli tilni tanlang"<<endl;
+cin>>ozbektili;
+
+    switch (ozbektili) {
+        case 1:
+            cout<<"\n\t\tParolni kiriting"<<endl;
+            cin>>parol;
+            if (parol==parol){cout<<"Bosh menyu"<<endl;}
+            else{cout<<"Parolni xato kiritdingiz qaytadan urinib koring"<<endl;
+            }
+    }  // bankomad*/
+#include <iostream>
+
+    using namespace std;
+
+    int main() {
+        int a,b;
+        cout <<"a:"<<"b:";
+        cin >>a>>b;
+
+        int s =a*b;
+        int  p =(a+b)*2;
+        cout <<"S:"<<s<<endl;
+        cout <<"P:"<<p<<endl;
+
+    return 0;
+
+}
